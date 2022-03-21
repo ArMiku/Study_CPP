e5c4f3b0ec126b245af250bb9b25e632aaa56666
sunqihang <qihang130681@gmail.com>
2022-03-21 15:22:41 +0800

    Practice of 3.1.

---
+++ Chapter III: Simple Variables/3.1.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 孙啟航 on 2022/3/21.
+//
+#include <iostream>
+
+using namespace std;
+
+const int feet2inch = 12;
+
+int main() {
+    double height = 68.5;
+    cout << height / feet2inch <<endl;
+    return 0;
+}
